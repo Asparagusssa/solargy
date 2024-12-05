9ac3fa1a9d0fe6ae9082d44941fdd3311f07dd84
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-12-05 23:48:12 +0400

    update

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function index(Request $request)
+    {
+        $attrs = request()->validate([
+            'email' => ['required', 'email', 'exists:users,email'],
+            'password' => ['required'],
+        ]);
+
+        if(! Auth::attempt($attrs) ) {
+            throw ValidationException::withMessages([
+                'error' => 'Не верный логин или пароль',
+            ])->status(401);
+        }
+
+        request()->session()->regenerate();
+
+        return response()->json('Авторизация прошла успешно', 200);
+    }
+}
