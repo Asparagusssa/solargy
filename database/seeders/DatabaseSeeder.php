d069d4effe84b64bba25739d15e238e9eb9d6791
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-12-10 12:16:04 +0400

    add delivery

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -55,6 +55,10 @@ class DatabaseSeeder extends Seeder
             [
                 'title' => 'about',
                 'url' => 'about',
+            ],
+            [
+                'title' => 'delivery',
+                'url' => 'delivery',
             ]
         ];
 
