3f2df23bf959e287da31f73993675f2d4c2e8e18
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-12-10 23:30:30 +0400

    update

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
@@ -16,14 +18,19 @@ class LoginController extends Controller
             'password' => ['required'],
         ]);
 
-        if(! Auth::attempt($attrs) ) {
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'error' => 'Не верный логин или пароль',
             ])->status(401);
         }
 
+        Auth::login($user);
+        $token = $user->createToken($request->email)->plainTextToken;
+
         request()->session()->regenerate();
 
-        return response()->json('Авторизация прошла успешно', 200);
+        return response()->json(['token' => $token], 200);
     }
 }
