f8be889e2987c6275058bdc1838851fec3d1a75e
maximghgh <maximbas2005@gmail.com>
2025-12-10 01:16:50 +0400

    Auto Archive

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,20 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Schedule;
+use App\Models\Promo;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    Promo::query()
+        ->whereNotNull('end')
+        ->where('end', '<', now())
+        ->where(function ($q) {
+            $q->whereNull('is_archived')
+              ->orWhere('is_archived', false);
+        })
+        ->update(['is_archived' => true]);
+})->hourly();
